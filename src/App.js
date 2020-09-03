1635898b350e5e6942f1e7aa5b8ddfe6f73421a5
Tran Quoc Trung <tranquoctrung.1610@gmail.com>
2020-09-03 13:11:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Docker Ubuntu
         </a>
       </header>
     </div>
